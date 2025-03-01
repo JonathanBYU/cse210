25dc8cb4f447dc820c715176841d4a842cf9ddc0
JonathanBYU <roi24003@byui.edu>
2025-02-28 21:13:10 -0700

    Complete Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,36 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName() {
+        Console.Write("Please enter your name: ");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
+
+    static int PromptUserNumber() {
+        Console.Write("Please enter your favorite number: ");
+        int number = Convert.ToInt16(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int num) {
+        return num * num;
+    }
+
+    static void DisplayResult(string user_name, int sqr_num) {
+        Console.WriteLine($"{user_name}, the square of your number is {sqr_num}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string user_name = PromptUserName();
+        int num = PromptUserNumber();
+        int sqr_num = SquareNumber(num);
+        DisplayResult(user_name,sqr_num);
     }
 }
