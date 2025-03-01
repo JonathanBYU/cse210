ece4faec5d0978512879e2d67ab912f9fec959a9
JonathanBYU <roi24003@byui.edu>
2025-02-28 20:55:14 -0700

    Complete Prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.Write("Enter number: ");
+        int number = Convert.ToInt16(Console.ReadLine());
+
+        while (number != 0) {
+            numbers.Add(number);
+            Console.Write("Enter number: ");
+            number = Convert.ToInt16(Console.ReadLine());
+        };
+
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
