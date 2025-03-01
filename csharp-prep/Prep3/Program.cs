8bfe0a21f6aa1d84fb95205dbc544bf6714c4d41
JonathanBYU <roi24003@byui.edu>
2025-02-28 20:36:41 -0700

    Prep3 Solution

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rand = new Random();
+        int number = rand.Next(1,101);
+        int guess;
+        int num_guesses = 0;
+        bool play_again;
+
+        do {
+            do {
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt16(Console.ReadLine());
+                if (number > guess) {
+                    Console.WriteLine("Higher");
+                } else if (number < guess) {
+                    Console.WriteLine("Lower");
+                } else {
+                    Console.WriteLine("You guessed it!");
+                }
+                num_guesses += 1;
+            } while (number != guess);
+
+            Console.WriteLine($"You made {num_guesses} guesses.");
+            Console.WriteLine("Would you like to play again?");
+            play_again = Console.ReadLine() == "yes";
+        } while (play_again);
     }
 }
