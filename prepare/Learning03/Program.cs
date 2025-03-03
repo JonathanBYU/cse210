91d57700e25de91c2feb128628c0db5862a6874d
JonathanBYU <roi24003@byui.edu>
2025-03-03 13:11:59 -0700

    Prepare assignment progress

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,5 +1,23 @@
 using System;
 
+class Job {
+    string company;
+    string jobTitle;
+    int startYear;
+    int endYear;
+    public void Display() {
+        Console.WriteLine($"{jobTitle} ({company}) {startYear}-{endYear}");
+    }
+}
+
+class Resume {
+    string name;
+    Job[] jobs;
+    public void Display() {
+
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
